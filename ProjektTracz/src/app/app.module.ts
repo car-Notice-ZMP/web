96b3fe5175f731b83125a6164ca2e3ad2273e330
not-raspy <54740570+not-raspy@users.noreply.github.com>
2021-02-25 19:09:44 +0100

    cloned private repo to organization
    done:
    style scss
    flex-layout

---
+++ ProjektTracz/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { FacebookComponent } from './components/facebook/facebook.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FacebookComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
